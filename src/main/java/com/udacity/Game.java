ae8711becf1cf1f25c8d431d75c3a679406f31cd
Klaudia <klaudias.nowak@gmail.com>
2019-07-13 22:12:17 +0200

    If's statements changed

--- src/main/java/com/udacity/Game.java
+++ src/main/java/com/udacity/Game.java
@@ -10,31 +10,31 @@ public class Game {
 
     private char turn; // who's turn is it, 'x' or 'o' ? x always starts
     private boolean twoPlayer; // true if this is a 2 player game, false if AI playing
-    private char [][] grid; // a 2D array of chars representing the game grid
+    private char[][] grid; // a 2D array of chars representing the game grid
     private int freeSpots; // counts the number of empty spots remaining on the board (starts from 9  and counts down)
     private static GameUI gui;
 
     /**
      * Create a new single player game
-     *
      */
     public Game() {
-        newGame(false);
+        newGame( false );
     }
 
     /**
      * Create a new game by clearing the 2D grid and restarting the freeSpots counter and setting the turn to x
+     *
      * @param twoPlayer: true if this is a 2 player game, false if playing against the computer
      */
-    public void newGame(boolean twoPlayer){
+    public void newGame(boolean twoPlayer) {
         //sets a game to one or two player
         this.twoPlayer = twoPlayer;
 
         // initialize all chars in 3x3 game grid to '-'
         grid = new char[3][3];
         //fill all empty slots with -
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 grid[i][j] = '-';
             }
         }
@@ -47,16 +47,17 @@ public class Game {
 
     /**
      * Gets the char value at that particular position in the grid array
+     *
      * @param i the x index of the 2D array grid
      * @param j the y index of the 2D array grid
      * @return the char value at the position (i,j):
-     *          'x' if x has played here
-     *          'o' if o has played here
-     *          '-' if no one has played here
-     *          '!' if i or j is out of bounds
+     * 'x' if x has played here
+     * 'o' if o has played here
+     * '-' if no one has played here
+     * '!' if i or j is out of bounds
      */
-    public char gridAt(int i, int j){
-        if(i>=3||j>=3||i<0||j<0)
+    public char gridAt(int i, int j) {
+        if (i >= 3 || j >= 3 || i < 0 || j < 0)
             return '!';
         return grid[i][j];
     }
@@ -64,16 +65,17 @@ public class Game {
     /**
      * Places current player's char at position (i,j)
      * Uses the variable turn to decide what char to use
+     *
      * @param i the x index of the 2D array grid
      * @param j the y index of the 2D array grid
      * @return boolean: true if play was successful, false if invalid play
      */
-    public boolean playAt(int i, int j){
+    public boolean playAt(int i, int j) {
         //check for index boundries
-        if(i>=3||j>=3||i<0||j<0)
+        if (i >= 3 || j >= 3 || i < 0 || j < 0)
             return false;
         //check if this position is available
-        if(grid[i][j] != '-'){
+        if (grid[i][j] != '-') {
             return false; //bail out if not available
         }
         //update grid with new play based on who's turn it is
@@ -86,22 +88,24 @@ public class Game {
 
     /**
      * Override
+     *
      * @return string format for 2D array values
      */
-    public String toString(){
-        return Arrays.deepToString(this.grid);
+    public String toString() {
+        return Arrays.deepToString( this.grid );
     }
 
     /**
      * Performs the winner chack and displayes a message if game is over
+     *
      * @return true if game is over to start a new game
      */
     public boolean doChecks() {
         //check if there's a winner or tie ?
-        String winnerMessage = checkGameWinner(grid);
-        if (!winnerMessage.equals("None")) {
-            gui.gameOver(winnerMessage);
-            newGame(false);
+        String winnerMessage = checkGameWinner( grid );
+        if (!winnerMessage.equals( "None" )) {
+            gui.gameOver( winnerMessage );
+            newGame( false );
             return true;
         }
         return false;
@@ -110,29 +114,27 @@ public class Game {
     /**
      * Allows computer to play in a single player game or switch turns for 2 player game
      */
-    public void nextTurn(){
+    public void nextTurn() {
         //check if single player game, then let computer play turn
-        if(!twoPlayer){
-            if(freeSpots == 0){
-                return ; //bail out if no more free spots
+        if (!twoPlayer) {
+            if (freeSpots == 0) {
+                return; //bail out if no more free spots
             }
             int ai_i, ai_j;
             do {
                 //randomly pick a position (ai_i,ai_j)
                 ai_i = (int) (Math.random() * 3);
                 ai_j = (int) (Math.random() * 3);
-            }while(grid[ai_i][ai_j] != '-'); //keep trying if this spot was taken
+            } while (grid[ai_i][ai_j] != '-'); //keep trying if this spot was taken
             //update grid with new play, computer is always o
             grid[ai_i][ai_j] = 'o';
             //update free spots
             freeSpots--;
-        }
-        else{
+        } else {
             //change turns
-            if(turn == 'x'){
+            if (turn == 'x') {
                 turn = 'o';
-            }
-            else{
+            } else {
                 turn = 'x';
             }
         }
@@ -145,40 +147,47 @@ public class Game {
      * If game hasn't ended the return string has to be "None",
      * If the game ends as tie, the return string has to be "Tie",
      * If the game ends because there's a winner, it should return "X wins" or "O wins" accordingly
+     *
      * @param grid 2D array of characters representing the game board
      * @return String indicating the outcome of the game: "X wins" or "O wins" or "Tie" or "None"
      */
-    public String checkGameWinner(char [][]grid) {
+    public String checkGameWinner(char[][] grid) {
         String result = "None";
+        boolean hasWon = false;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if ((grid[i][0] == 'x' && grid[i][1] == 'x' && grid[i][2] == 'x') || (grid[0][j] == 'x' && grid[1][j] == 'x' && grid[2][j] == 'x') || (grid[0][0] == 'x' && grid[1][1] == 'x' && grid[2][2] == 'x') || (grid[0][2] == 'x' && grid[1][1] == 'x' && grid[2][0] == 'x')) {
                     result = "X wins";
-                }
-
-                if ((grid[i][0] == 'o' && grid[i][1] == 'o' && grid[i][2] == 'o') || (grid[0][j] == 'o' && grid[1][j] == 'o' && grid[2][j] == 'o') || (grid[0][0] == 'o' && grid[1][1] == 'o' && grid[2][2] == 'o') || (grid[0][2] == 'o' && grid[1][1] == 'o' && grid[2][0] == 'o')) {
+                    break;
+                } else if ((grid[i][0] == 'o' && grid[i][1] == 'o' && grid[i][2] == 'o') || (grid[0][j] == 'o' && grid[1][j] == 'o' && grid[2][j] == 'o') || (grid[0][0] == 'o' && grid[1][1] == 'o' && grid[2][2] == 'o') || (grid[0][2] == 'o' && grid[1][1] == 'o' && grid[2][0] == 'o')) {
                     result = "O wins";
+                    break;
 
-                }
-                if (result != "X wins" && result != "O wins" && grid [0][0] != '-' && grid [1][0] != '-' && grid [2][0] != '-' && grid [0][1] != '-' && grid [1][1] != '-' && grid [2][1] != '-' && grid [0][2] != '-' && grid [1][2] != '-' && grid [2][2] != '-' ){
+                } else if (result != "X wins" && result != "O wins" && grid[0][0] != '-' && grid[1][0] != '-' && grid[2][0] != '-' && grid[0][1] != '-' && grid[1][1] != '-' && grid[2][1] != '-' && grid[0][2] != '-' && grid[1][2] != '-' && grid[2][2] != '-') {
                     result = "Tie";
+                    break;
+                } else {
+                    return result;
                 }
 
             }
+
         }
-                //Student code goes here ...
-                return result;
+
+        //Student code goes here ...
+        return result;
 
     }
 
     /**
      * Main function
+     *
      * @param args command line arguments
      */
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Game game = new Game();
-        gui = new GameUI(game);
+        gui = new GameUI( game );
     }
 
 }
